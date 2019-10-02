0af3cf72756c18c2b4f5eb2af9149504088327e9
Nadzeya Paulava <nadezhda.zuy@gmail.com>
2019-10-03 01:33:31 +0300

    brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    const regExpSeparator = new RegExp(bracketsConfig.map(aBracket => {
+        return aBracket.map(oBracket => {
+            if (!Number.isInteger(parseInt(oBracket, 10))) {
+                return '\\' + oBracket;
+            }
+            return oBracket;
+        }).join('')
+    }).join('\|'), 'g');
+    return !(replaceBracket(str, regExpSeparator).length);
+}
+
+function replaceBracket(str, regExpSeparator) {
+    if (str.match(regExpSeparator)) {
+        return replaceBracket(str.replace(regExpSeparator, ''), regExpSeparator);
+    }
+    return str;
 }
